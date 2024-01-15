4a07c6c1977cae854aea6d063f932ab0de0c99e2
Cem <122365538+fcemyuksel@users.noreply.github.com>
2024-01-15 11:28:50 +0300

    Update TrelloTest.java

--- TestiniumTrelloApiCaseStudy/src/main/java/org/trello/TrelloTest.java
+++ TestiniumTrelloApiCaseStudy/src/main/java/org/trello/TrelloTest.java
@@ -1,98 +1,87 @@
 package org.trello;
 
+import org.testng.annotations.Test;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.junit.Test;
+import org.junit.jupiter.api.Order;
 import utilities.ConfigReader;
+
 
 public class TrelloTest {
 
-
-    String APIKey = "84fc4150414f99009bcdba78f02fa3ee";
-    String APIToken = "ATTAed1feee91d3e5479daa56d0bc7b43b4b01b3b71df363baa147bdbf60ed72ddec34D71F34";
-    String boardName = "Testinium Board";
-    String listName = "Testinium List";
-    String cardName1 = "Testinium Card1";
-    String cardName2 = "Testinium Card2";
-
-
-
-    @Test
-        //createTrelloBoard
-    public void a() throws UnirestException, InterruptedException {
-
+    @Test (priority = 1)
+    //createTrelloBoard
+    public void createTrelloBoard() throws UnirestException{
         HttpResponse<String> response = Unirest.post("https://api.trello.com/1/boards/")
-                .queryString("name", boardName)
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("name", ConfigReader.getProperty("boardName"))
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asString();
 
         System.out.println(response.getBody());
-        JsonObject jsonResponse = JsonParser.parseString(response.getBody().toString()).getAsJsonObject();
+        JsonObject jsonResponse = JsonParser.parseString(response.getBody()).getAsJsonObject();
         ConfigReader.setProperty("boardId", jsonResponse.get("id").getAsString());
     }
 
-    @Test
+
+    @Test (priority = 2)
     //createTrelloListOnBoard
-    public void b() throws UnirestException, InterruptedException {
-        Thread.sleep(2500);
+    public void createTrelloListOnBoard() throws UnirestException, InterruptedException {
+        Thread.sleep(500);
         HttpResponse<String> response = Unirest.post("https://api.trello.com/1/lists")
-                .queryString("name", listName)
+                .queryString("name", ConfigReader.getProperty("listName"))
                 .queryString("idBoard", ConfigReader.getProperty("boardId"))
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asString();
 
         System.out.println(response.getBody());
-        JsonObject jsonResponse = JsonParser.parseString(response.getBody().toString()).getAsJsonObject();
+        JsonObject jsonResponse = JsonParser.parseString(response.getBody()).getAsJsonObject();
         ConfigReader.setProperty("listId", jsonResponse.get("id").getAsString());
-
     }
 
 
-    @Test
+    @Test (priority = 3)
     //createTrelloCard1OnList
-    public void c() throws UnirestException, InterruptedException {
-        Thread.sleep(2500);
+    public void createTrelloCard1OnList() throws UnirestException, InterruptedException {
+        Thread.sleep(500);
         HttpResponse<JsonNode> response = Unirest.post("https://api.trello.com/1/cards")
                 .header("Accept", "application/json")
-                .queryString("name", cardName1)
+                .queryString("name", ConfigReader.getProperty("cardName1"))
                 .queryString("idList", ConfigReader.getProperty("listId"))
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asJson();
 
         JsonObject jsonResponse = JsonParser.parseString(response.getBody().toString()).getAsJsonObject();
         ConfigReader.setProperty("cardId1", jsonResponse.get("id").getAsString());
-
     }
 
-    @Test
+    @Test (priority = 4)
     //createTrelloCard2OnList
-    public void d() throws UnirestException, InterruptedException {
-        Thread.sleep(2500);
+    public void createTrelloCard2OnList() throws UnirestException, InterruptedException {
+        Thread.sleep(500);
         HttpResponse<JsonNode> response = Unirest.post("https://api.trello.com/1/cards")
                 .header("Accept", "application/json")
-                .queryString("name", cardName2)
+                .queryString("name", ConfigReader.getProperty("cardName2"))
                 .queryString("idList", ConfigReader.getProperty("listId"))
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asJson();
 
         JsonObject jsonResponse = JsonParser.parseString(response.getBody().toString()).getAsJsonObject();
         ConfigReader.setProperty("cardId2", jsonResponse.get("id").getAsString());
-
-
     }
 
-    @Test
+
+    @Test (priority = 5)
     //updateTrelloCardOnListRandomly
-    public void e() throws UnirestException, InterruptedException {
-        Thread.sleep(2500);
+    public void updateTrelloCardOnListRandomly() throws UnirestException, InterruptedException {
+        Thread.sleep(500);
         String cardID;
         int tmp = (int) (Math.random() * 1) + 1;
         if (tmp == 1) {
@@ -105,42 +94,43 @@ public class TrelloTest {
         HttpResponse<JsonNode> response = Unirest.put(urlUpdated)
                 .header("Accept", "application/json")
                 .queryString("id", cardID)
-                .queryString("name", "Updated Card")
+                .queryString("name", "Trello Card Updated")
                 .queryString("color", "blue")
                 .queryString("idList", ConfigReader.getProperty("listId"))
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asJson();
-    }
 
-    @Test
-    //deleteTrelloCardOnList
-    public void f() throws UnirestException, InterruptedException {
-        Thread.sleep(2500);
+    }
+    @Test (priority = 6)
+   //deleteTrelloCardOnList
+    public void deleteTrelloCardOnList() throws UnirestException, InterruptedException {
+        Thread.sleep(500);
         HttpResponse<String> response = Unirest.delete("https://api.trello.com/1/cards/" + ConfigReader.getProperty("cardId1"))
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asString();
     }
 
+    @Test (priority = 7)
     //deleteTrelloCard2OnList
-    public void g() throws UnirestException, InterruptedException {
-        Thread.sleep(2500);
+    public void deleteTrelloCard2OnList() throws UnirestException, InterruptedException {
+        Thread.sleep(500);
         HttpResponse<String> response = Unirest.delete("https://api.trello.com/1/cards/" + ConfigReader.getProperty("cardId2"))
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asString();
+
     }
 
-    @Test
+    @Test (priority = 8)
     //deleteTrelloBoard
-    public void h() throws UnirestException, InterruptedException {
-        Thread.sleep(2500);
+    public void deleteTrelloBoard() throws UnirestException, InterruptedException {
+        Thread.sleep(500);
         HttpResponse<String> response = Unirest.delete("https://api.trello.com/1/boards/" + ConfigReader.getProperty("boardId"))
-                .queryString("key", APIKey)
-                .queryString("token", APIToken)
+                .queryString("key", ConfigReader.getProperty("APIKey"))
+                .queryString("token", ConfigReader.getProperty("APIToken"))
                 .asString();
 
     }
 }
-
